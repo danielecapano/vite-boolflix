d5759068cb105181ceb6cc585530fc0a2b7db10b
danielecapano <daniele.capano@gmail.com>
2023-11-10 13:25:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import axios from 'axios'
+import { store } from './store'
+
+export default {
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      store
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { store } from '../store'
+    export default {
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
